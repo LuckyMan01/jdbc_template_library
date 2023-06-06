7a45e2972dce7c3cb534e0d26a08dd68456f5aeb
Volodymyr <wolodjag@ukr.net>
2023-06-06 15:36:21 +0200

    add implementing dto and controlers

--- src/main/java/com/example/library/moduls/Person.java
+++ src/main/java/com/example/library/moduls/Person.java
@@ -1,2 +1,48 @@
-package com.example.library.moduls;public class Person {
+package com.example.library.moduls;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private int id;
+    @NotEmpty(message = "name should not be empty")
+    @Size(min = 2, max = 100, message = "name should be 2 or 100 characters")
+    private String fullName;
+
+    @Min(value = 0, message = "Age should be greater then 0")
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(int id, String fullName, int age) {
+        this.id = id;
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
